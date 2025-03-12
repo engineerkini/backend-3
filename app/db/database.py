c405625ce129dea4156974b492b33c22b45780cc
engineerkini <engineerkini@gmail.com>
2025-03-12 12:44:55 -0400

    intial commit

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://user:password@localhost/spotify_db"
+DATABASE_URL = "postgresql+psycopg2://user:password@localhost/spotify_db"
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
