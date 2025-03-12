078b087067168c30c27ef7f998226b61cfa0d0b9
engineerkini <engineerkini@gmail.com>
2025-03-12 13:28:52 -0400

    url

--- app/db/database.py
+++ app/db/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "postgresql+psycopg2://user:password@localhost/spotify_db"
+DATABASE_URL = "postgresql+asyncpg://user:password@localhost/spotify_db"
 
 
 engine = create_engine(DATABASE_URL)
